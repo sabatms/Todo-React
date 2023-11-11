0367aae07e5fdd9f1e6c6ac64c6d22fc933ad93b
sabatms <saba.tms15@gmail.com>
2023-11-10 21:46:24 -0800

    add app.js file

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function Product(){
+  
+}
+
+
+function print(){
+  console.log("hello from print")
+}
+          
+print()
+
 export function App() {
-          return <h1>Hello world!</h1>;
+return (
+<div>
+<h1>Hello Saba.Tms1</h1>
+<input type="text" placeholder="search.." />
+</div>
+)
 }
