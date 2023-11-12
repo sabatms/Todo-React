6cefdbd88a74c3f099ecff10cae3b6194b1f5e5a
sabatms <saba.tms15@gmail.com>
2023-11-12 04:17:05 -0800

    add TodoInput.js file

---
+++ src/components/Todoinput.js
@@ -0,0 +1,9 @@
+function TodoInput(){
+          return(
+          <div>
+          <input type="text" placeholder="Write your Todo.." id="title"/>
+          <button id="save-btn">Save</button>
+          </div>
+          );
+}
+export default TodoInput;
