aa9fc114777c8cdbe76012bb181289f32c37d977
sabatms <saba.tms15@gmail.com>
2023-11-12 04:15:39 -0800

    add App.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-function Product(){
-  
-}
+import Actions from "./components/Actions";
+import Todo from "./components/Todo";
+import TodoInput from "./components/Todoinput";
+import Actions from "./components/Actions";
 
+const todos = [
+  {
+    title: "item1",
+    status:false,
+  },
+  {
+    title: "item2",
+    status:true,
+  },
+  {
+    title: "item3",
+    status:true,
+  },
+];
 
-function print(){
-  console.log("hello from print")
-}
-          
-print()
+// todos[0].title
+// todos[1].title
+// todos[2].title
 
 export function App() {
-return (
-<div>
-<h1>Hello Saba.Tms1</h1>
-<input type="text" placeholder="search.." />
-</div>
-)
+  return (
+    <div className="container">
+    <div className="form">
+      <TodoInput />
+        <br />
+        <br />
+      <Actions />
+    </div>
+    <div className="list">
+      <Todo title ={todos[0].title} status ={todos[0].status} />
+      <Todo title ={todos[1].title} status ={todos[1].status}/>
+      <Todo title ={todos[2].title} status ={todos[2].status}/>
+      </div>
+    </div> 
+  );
 }
