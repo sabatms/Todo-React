41b2a799e7ff7fb6d92bc737e5a9f0d6681ee8b3
sabatms <saba.tms15@gmail.com>
2023-11-13 22:44:11 -0800

    delete this file

--- src/components/Todoinput.js
+++
@@ -1,9 +0,0 @@
-function TodoInput(){
-          return(
-          <div>
-          <input type="text" placeholder="Write your Todo.." id="title"/>
-          <button id="save-btn">Save</button>
-          </div>
-          );
-}
-export default TodoInput;
