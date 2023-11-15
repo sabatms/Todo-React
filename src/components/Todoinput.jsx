109b46f7aee84cfa90c54590869467bb23375cec
sabatms <saba.tms15@gmail.com>
2023-11-14 23:44:44 -0800

    add Todoinput.jsx file

--- src/components/Todoinput.jsx
+++ src/components/Todoinput.jsx
@@ -1,8 +1,18 @@
+function onAddItem(){
+          console.log("clicked")
+}
+
+function onType(e){
+          const char = e.key
+          console.log(char )
+}
+
+
 function TodoInput(){
           return(
           <div>
-          <input type="text" placeholder="Write your Todo.." id="title"/>
-          <button id="save-btn">Save</button>
+          <input   onKeyUp={onType} type="text" placeholder="Write your Todo.." id="title" />
+          <button  onClick={onAddItem} id="save-btn">Save</button>
           </div>
           );
 }
