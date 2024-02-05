b70d9fa6835cdc61d12ca737fd16c4923b885e7e
Saba Tahmasebi <saba.tms15@gmail.com>
2024-02-05 08:24:33 -0800

    array.js

---
+++ array.js
@@ -0,0 +1,17 @@
+const my_array = [10, 20, 30];
+
+console.log(my_array[0]);
+console.log(my_array[1]);
+console.log(my_array[2]);
+
+const location = [59.1111, 36.789];
+const lat = location[0]
+const long = location[1]
+
+
+const info = ["saba" , 25]
+const name = info[0]
+const age = info[1]
+
+console.log(name)
+console.log(age)
