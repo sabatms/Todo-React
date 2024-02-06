773cce130c4b6220aae7b8f55e2ae089be1c0068
Saba Tahmasebi <saba.tms15@gmail.com>
2024-02-06 08:49:14 -0800

    saba.js

--- saba.js
+++ saba.js
@@ -1,15 +1,25 @@
-function useState2() {
-  const value = "";
-  const updateValue = (next_value) => {
-    value = next_value;
-  };
-
-  return [value, updateValue];
+function render (){
+  console.log("renderd")
 }
 
-const my_folan = useState2();
+
+function useState() {
+  let state = {
+  value : "folan" ,
+  };
+  const updateValue = (next_value) => {
+    state.value = next_value;
+    render()
+  };
+
+  return [state, updateValue];
+}
+
+const my_folan = useState();
 const folan = my_folan[0];
 const updateFolan = my_folan[1];
 
-console.log(folan);
+console.log(folan.value);
+updateFolan("bisar");
 
+console.log(folan.value);
