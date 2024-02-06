a5c8a7b271e41bd9e21a6495fc2620fee27e6018
Saba Tahmasebi <saba.tms15@gmail.com>
2024-02-06 08:50:45 -0800

    src

--- src/components/Todoinput.jsx
+++ src/components/Todoinput.jsx
@@ -4,13 +4,10 @@ function onAddItem() {
           console.log("clicked")
 }
 
-let my_string = "folan";
+//let my_string = "folan";
 
-function TodoInput() {
-const my_string_react = useState("folan");
-
-
-
+function TodoInput(props) {
+const [my_string , update_string] = useState("");
           return (
           <div>
           <h1> Your todo title : {my_string} </h1>
@@ -18,15 +15,18 @@ const my_string_react = useState("folan");
           <input 
           onKeyUp={(e) =>{
           const char= e.key
-          my_string = my_string + char;
+         // my_string = my_string + char;
 
-          console.log(my_string)
+         update_string(my_string + char)
           }} 
           type="text"
           placeholder="Write your todo ..."
           id="title"
           />
-          <button onClick={onAddItem} id="save-btn">
+          <button onClick={() =>{
+
+             props.handleSubmit(my_string)
+          }} id="save-btn">
            Save
           </button>
           </div>
