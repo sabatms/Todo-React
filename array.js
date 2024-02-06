2b8503a5cf3b51db5b3749e2458652b840506aa8
Saba Tahmasebi <saba.tms15@gmail.com>
2024-02-06 08:49:00 -0800

    array.js

--- array.js
+++ array.js
@@ -1,17 +1,34 @@
-const my_array = [10, 20, 30];
+// const my_array = [10, 20, 30];
 
-console.log(my_array[0]);
-console.log(my_array[1]);
-console.log(my_array[2]);
-
-const location = [59.1111, 36.789];
-const lat = location[0]
-const long = location[1]
+// console.log(my_array[0]);
+// console.log(my_array[1]);
+// console.log(my_array[2]);
 
 
-const info = ["saba" , 25]
-const name = info[0]
-const age = info[1]
+const info = ["saba", 25];
+const name = info[0];
+const age = info[1];
 
-console.log(name)
-console.log(age)
+// console.log(name)
+// console.log(age)
+
+
+// const location = [59.1111, 36.789];
+// const lat = location[0];
+// const long = location[1];
+
+// function generate() {
+//    const l = 10;
+//    const n = 20;
+
+//    return [l,n]
+// }
+
+// const output = generate();
+
+// const[l,n] = output
+
+const location2 = [59.1111, 36.789];
+const [lat, long] = location2;
+console.log(lat)
+console.log(long)
