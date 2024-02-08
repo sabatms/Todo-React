82a52a22665658db5dd78dc83ffe41ef75894aeb
Saba Tahmasebi <saba.tms15@gmail.com>
2024-02-07 21:48:53 -0800

    var.js

---
+++ src/var.js
@@ -0,0 +1,13 @@
+let my_name = "saba";
+let info = {
+  country : "folan",
+  city :"bisar",
+};
+
+let status = false;
+let empty = null;
+
+my_name = "arsam";
+status = true;
+
+console.log(my_name);
