7f05203a9b86e538a2667b3648a070aa67eceab4
Saba Tahmasebi <saba.tms15@gmail.com>
2024-02-07 21:48:13 -0800

    src

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,7 @@ import Todo from "./components/Todo";
 import TodoInput from "./components/Todoinput";
 import Actions from "./components/Actions";
 
-const todos = [
-  {
-    title: "item1 --- 0",
-    status:false,
-  },
-  {
-    title: "item2",
-    status:true,
-  },
-  {
-    title: "item3",
-    status:true,
-  },
-  {
-    title: "item4",
-    status:false,
-  },
-  {
-    title: "item5",
-    status:false,
-  },
-];
+
 
 export function App() {
   const [list , setList] = useState([]) ;
